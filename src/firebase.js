4f22c10a683577edc9e078e766fd822ce5d68818
Gauravarora017 <arora2000gaurav@gmail.com>
2023-06-16 22:18:53 +0530

    login

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // import firebase from "firebase/app"
+// import {getauth} from "firebase"
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCDLuBZvJrBSwdTereEG6KlcaenQAIR_FY",
+//   authDomain: "bookshala-b9359.firebaseapp.com",
+//   projectId: "bookshala-b9359",
+//   storageBucket: "bookshala-b9359.appspot.com",
+//   messagingSenderId: "696158743601",
+//   appId: "1:696158743601:web:0538742b9985b2a7c7b910",
+//   measurementId: "G-72T6T8C3ZV"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// const auth = getauth();
+
+// export {app };
